9fc5066ecb8e76422e372d69ba1417d96ff29e12
top329 <tanakayuki329@outlook.com>
2024-04-16 00:41:49 +0800

    update: API

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning={true} className={`${catFont.variable} font-sans`}>
+    <html lang="en" suppressHydrationWarning={true} className={`${catFont.variable}`}>
       <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
       </head>
